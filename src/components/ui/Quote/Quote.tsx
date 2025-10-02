f3491e2f6916cf752716208f05fdb2b08f157a66
yuriipohorilets <pohorilets.y@gmail.com>
2025-10-02 11:48:17 +0300

    added quote section

--- src/components/ui/Quote/Quote.tsx
+++ src/components/ui/Quote/Quote.tsx
@@ -4,10 +4,11 @@ import styles from './Quote.module.scss';
 interface QuoteProps extends React.PropsWithChildren {
   align: 'start' | 'center' | 'end';
   uppercase?: boolean;
+  className?: string;
 }
 
-export const Quote: React.FC<QuoteProps> = ({ children, align, uppercase }) => {
-  const classNames = clsx(styles.quote, styles[align], uppercase && styles.uppercase);
+export const Quote: React.FC<QuoteProps> = ({ children, align, uppercase, className }) => {
+  const classNames = clsx(styles.quote, styles[align], uppercase && styles.uppercase, className);
 
   return <p className={classNames}>{children}</p>;
 };
--- src/scenes/home/Quotes/Quotes.tsx
+++ src/scenes/home/Quotes/Quotes.tsx
@@ -1,5 +1,30 @@
-import React from 'react';
+import { Container, IconWrapper, Quote, Section } from '@/components/ui';
+import { CrownIcon } from '@/components/icons';
+import styles from './Quotes.module.scss';
+
+const content = {
+  title: 'Advantage',
+  quote:
+    'Bachelor bunny gourmet delivers convenient and luxurious home-style meals\n to your doorstep',
+};
 
 export const Quotes = () => {
-  return <div>Quotes</div>;
+  return (
+    <Section className={styles.section}>
+      <Container fullHeight>
+        <div className={styles.wrapper}>
+          <h2 className="hidden">{content.title}</h2>
+
+          <div className={styles.logo}>
+            <IconWrapper Icon={CrownIcon} className={styles.icon} />
+            <span className={styles.label}>BBG</span>
+          </div>
+
+          <Quote align="center" uppercase className={styles.quote}>
+            {content.quote}
+          </Quote>
+        </div>
+      </Container>
+    </Section>
+  );
 };
